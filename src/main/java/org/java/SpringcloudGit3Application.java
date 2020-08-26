ebddaae19a40e7564a74b68be97abd8755e7b4ef
2729224868 <2729224868@qq.com>
2020-08-26 19:52:22 +0800

    第一次

---
+++ src/main/java/org/java/SpringcloudGit3Application.java
@@ -0,0 +1,13 @@
+package org.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudGit3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudGit3Application.class, args);
+    }
+
+}
